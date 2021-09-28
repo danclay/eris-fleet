74c5570f053f2391c48b7a672f5570e483a364f3
danclay <18021681+danclay@users.noreply.github.com>
2021-09-28 01:29:12 -0700

    Remove untracked files, update dependencies

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { Admiral as Fleet } from "./sharding/Admiral";
-export { BaseClusterWorker } from "./clusters/BaseClusterWorker";
-export { BaseServiceWorker } from "./services/BaseServiceWorker";
-export { Collection } from "./util/Collection";
-export { Setup as BaseClusterWorkerSetup } from "./clusters/BaseClusterWorker";
-export { Setup as BaseServiceWorkerSetup } from "./services/BaseServiceWorker";
-export { Options, Stats, ClusterStats, ShardStats, ServiceStats, ObjectLog, StartingStatus, ReshardOptions, LoggingOptions, ClusterCollection, ServiceCollection } from "./sharding/Admiral";
-export { IPC } from "./util/IPC";
