1dbe872f9e4247aeb387597134b7b0e144f30cc7
danclay <18021681+danclay@users.noreply.github.com>
2022-04-17 01:50:01 -0700

    Clean up
    - remove useless code from examples
    - update version number for publish

--- test/basicUsingModules/index.mjs
+++ test/basicUsingModules/index.mjs
@@ -1,16 +1,10 @@
 import {isMaster} from "cluster"
 import {Fleet} from "../../dist/index.js"
-import path from "path"
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 import {inspect} from "util"
 import {BotWorker} from "./bot.mjs"
 
 import dotenv from "dotenv"
 dotenv.config()
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 const options = {
     BotWorker: BotWorker,
