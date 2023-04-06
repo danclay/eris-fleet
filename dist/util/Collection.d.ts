637fd65476bc4adbec99821ae442d0d2bcef5e0c
danclay <18021681+danclay@users.noreply.github.com>
2023-04-06 03:14:08 -0700

    Revert "Add dist"
    This reverts commit a89d48198f6eb88e4ea23588e0c46d139f7dbde5.

--- dist/util/Collection.d.ts
+++
@@ -1,5 +0,0 @@
-/** Collection-ish */
-export declare class Collection<KeyType, ValueType> extends Map<KeyType, ValueType> {
-    constructor(iterable?: never);
-    find(func: (item: ValueType) => boolean): ValueType | undefined;
-}
